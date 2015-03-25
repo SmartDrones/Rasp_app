d837236478bc9cac9067f15813223e78fc0805f8
Alexandre Theve <alexandre.theve@davidson.fr>
2015-03-25 21:30:52 +0000

    ttyAMA0 instead of ttyO3

--- rasp_proxy.js
+++ rasp_proxy.js
@@ -1,7 +1,7 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("stty -F /dev/ttyO3 9600", puts);
+exec("stty -F /dev/ttyAMA0 9600", puts);
 
 var serial = require('serialport');
 var SerialPort = serial.SerialPort;
